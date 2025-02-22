3d4b01e859c98119bc0979d5cda1f9c16a815abe
Jona Baez <contactoconingenium@gmail.com>
2025-02-21 20:04:00 -0600

    edicion 4

--- base-ts-meta-memory/src/templates/mainFlow.ts
+++ base-ts-meta-memory/src/templates/mainFlow.ts
@@ -1,6 +1,6 @@
 import { addKeyword, EVENTS } from "@builderbot/bot"
 
 const mainFlow = addKeyword([EVENTS.WELCOME])
-    .addAnswer("Hola, bienvenido al *Chatbot*, pronto será productivo")
+    .addAnswer("Hola, bienvenido al *Chatbot*, pronto será productivo :)")
 
 export { mainFlow };
