c46bacdc89ea3c985f499ca75bdb125f44e03959
Jona Baez <contactoconingenium@gmail.com>
2025-02-23 15:10:29 -0600

    ediciones 4

--- base-ts-meta-memory/src/templates/index.ts
+++ base-ts-meta-memory/src/templates/index.ts
@@ -1,8 +1,8 @@
 import { createFlow } from "@builderbot/bot";
 import { mainFlow } from "./mainFlow";
 import { menuFlow } from "./menuFlow";
-import { sendPdfFlow } from "./list_templates/sendPdfFlow";
 import { sucursalesFlow } from "./list_templates/sucursalesFlow";
+import { sendPdfFlow } from "./list_templates/sendPdfFlow";
 import { sendVoiceFlow } from "./list_templates/sendVoiceFlow";
 import { sendImageFlow } from "./list_templates/sendimageFlow";
 
