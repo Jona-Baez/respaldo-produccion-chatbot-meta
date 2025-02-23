3fca3524d730dd871f320bc6e767e1a54bdf2db2
Jona Baez <contactoconingenium@gmail.com>
2025-02-23 11:53:03 -0600

    Se crea sucursalesFlow

--- base-ts-meta-memory/src/templates/list_templates/sucursalesFlow.ts
+++ base-ts-meta-memory/src/templates/list_templates/sucursalesFlow.ts
@@ -1,8 +1,5 @@
 import { addKeyword } from "@builderbot/bot";
 
 const sucursalesFlow= addKeyword("sucursales")
-    .addAnswer("Da clic en en enlace de la sucursale que te ineteresa acudir", {
-        media: "./assets/logo.png"
-    })
-
+    .addAnswer("Da clic en en enlace de la sucursale que te interesa acudir:")
 export { sucursalesFlow};
