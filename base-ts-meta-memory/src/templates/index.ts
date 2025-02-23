9a80adf212a25976690b2eac027772ca37043e09
Jona Baez <contactoconingenium@gmail.com>
2025-02-23 09:08:59 -0600

    Se agregan list_templates y menuFlow.ts

--- base-ts-meta-memory/src/templates/index.ts
+++ base-ts-meta-memory/src/templates/index.ts
@@ -1,9 +1,9 @@
 import { createFlow } from "@builderbot/bot";
 import { mainFlow } from "./mainFlow";
 import { menuFlow } from "./menuFlow";
-import { sendImageFlow } from "./list_templates/sendimageFlow";
 import { sendPdfFlow } from "./list_templates/sendPdfFlow";
 import { sendVoiceFlow } from "./list_templates/sendVoiceFlow";
+import { sendImageFlow } from "./list_templates/sendimageFlow";
 
 export default createFlow ([
     mainFlow,
